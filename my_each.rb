759bb2e6d5d6a18cce2a5b83a1bf69aaa2df5047
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-07 15:10:03 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,8 +2,7 @@ require 'pry'
 
 def my_each(collection)
   counter = 0
-  while collection.size > 0 do
-    yield element
+  while collection.size > 0 do {yield |element|}
     counter += 1
   end
 end
