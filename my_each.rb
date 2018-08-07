d5478734077191b0e858fa82ca81c6c05d241c90
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-07 14:55:47 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(collection)
   counter = 0
   while collection.size > 0 do
-    yield (|collection| puts collection)
+    yield {|collection| puts collection}
     counter += 1
   end
 end
